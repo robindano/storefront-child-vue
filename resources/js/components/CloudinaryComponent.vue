0fc66f022eeba682060bc7f6bec7ad7d6e1a1bf1
robindano <151894372+robindano@users.noreply.github.com>
2020-05-19 16:39:55 -0700

    start cloudinary component

--- resources/js/components/CloudinaryComponent.vue
+++ resources/js/components/CloudinaryComponent.vue
@@ -1,17 +1,59 @@
 <template>
   <div>
-    <cld-image cloudName="flaunt-your-site" publicId="WilliamBay-PartsPerMillion_DSC5995_lit9ko" crop="scale" width="300" />
     <cld-context cloudName="flaunt-your-site">
-      <cld-image publicId="WilliamBay-PartsPerMillion_DSC5995_lit9ko">
-        <cld-transformation crop="scale" width="200" angle="20" />
+      <cld-image
+        publicId="WilliamBay-PartsPerMillion_DSC5995_lit9ko"
+        :angle="angle"
+      >
+        <!-- <cld-transformation :angle="angle" /> -->
       </cld-image>
     </cld-context>
+
+    <div>
+      <button @click="rotate">ROTATE</button>
+    </div>
+
+    <div class="image-grid">
+      <div v-for="(image, index) in images" :key="index">
+        <img
+          class="image-grid__image"
+          :src="image"
+          @click="currentImage(index)"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      images: this.$root.cloudinaryUrls,
+      angle: 0
+    }
+  },
+  methods: {
+    currentImage(index) {
+      console.log(this.images[index])
+    },
+    rotate() {
+      // Logic to rotate at 45 degrees
+      this.angle = 90
+    }
+  }
+}
 </script>
 
-<style>
+<style scoped>
+.image-grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+  margin-top: 2rem;
+}
+
+.image-grid__image {
+  width: 100%;
+}
 </style>
