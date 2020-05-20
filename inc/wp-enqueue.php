db5f2b82bf730266449c416d0e51b4bf445291fe
robindano <151894372+robindano@users.noreply.github.com>
2020-05-19 19:37:19 -0700

    remove unused enqueue and conditionally add vue enqueue

--- inc/wp-enqueue.php
+++ inc/wp-enqueue.php
@@ -4,8 +4,10 @@ add_action('wp_enqueue_scripts', function () {
     $version = md5_file(get_stylesheet_directory() . '/mix-manifest.json');
 
     wp_enqueue_style('woo_photo', get_stylesheet_directory_uri() . '/dist/css/app.css', [], $version);
-    wp_enqueue_script('woo_vue', get_stylesheet_directory_uri() . '/dist/js/vue.js', ['jquery'], $version, true);
-    wp_enqueue_script('woo_photo', get_stylesheet_directory_uri() . '/dist/js/app.js', ['jquery', 'woo_vue'], $version, true);
+
+    if (is_product() || is_cart() && !WC()->cart->is_empty()) {
+        wp_enqueue_script('woo_photo', get_stylesheet_directory_uri() . '/dist/js/app.js', ['jquery'], $version, true);
+    }
 });
 
 add_action('admin_enqueue_scripts', function () {
