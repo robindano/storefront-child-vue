71df3236a28bb707dd719046b6bd25c1a1e8a5b7
robindano <151894372+robindano@users.noreply.github.com>
2020-05-19 18:42:20 -0700

    Removed full urls, and applied dynamic transformation to grid.

--- resources/js/components/CloudinaryComponent.vue
+++ resources/js/components/CloudinaryComponent.vue
@@ -10,11 +10,16 @@
       <button @click="rotate">ROTATE</button>
     </div>
 
-    <div class="image-grid">
-      <div class="image-grid-thumb" v-for="(image, index) in images" :key="index">
-        <img :src="image" @click="setCurrentImage(index)" />
-      </div>
-    </div>
+    <cld-context class="image-grid" cloudName="flaunt-your-site">
+      <cld-image
+        class="image-grid-thumb"
+        v-for="(image, index) in images"
+        :key="index"
+        :publicId="image"
+      >
+        <cld-transformation height="300" width="300" crop="fill" />
+      </cld-image>
+    </cld-context>
   </div>
 </template>
 
@@ -55,20 +60,15 @@ export default {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   grid-template-rows: 1fr;
-  grid-gap: 1rem;
+  grid-gap: 0.5rem;
   margin-top: 2rem;
+  padding: 0.5em;
+  border: 1px solid #666;
 }
 
 .image-grid-thumb {
   background: black;
   display: grid;
-}
-
-.image-grid-thumb:before {
-  content: "";
-  padding-bottom: 100%;
-  display: block;
-  grid-area: 1 / 1 / 2 / 2;
 }
 
 .image-grid-thumb img {
