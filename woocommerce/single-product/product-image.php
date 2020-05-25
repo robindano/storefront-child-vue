54a25c3c9d9283021d76b50a5448b21369ac724c
robindano <151894372+robindano@users.noreply.github.com>
2020-05-25 09:24:59 -0700

    detect product type before loading editor just in case it's a regular woo product. plus pass what type it is to vue.

--- woocommerce/single-product/product-image.php
+++ woocommerce/single-product/product-image.php
@@ -36,8 +36,28 @@ $wrapper_classes   = apply_filters(
         'images',
     ]
 );
+
+$product_types = ['Exhibition Prints', 'Posters', 'Outdoor/Vinyl Prints', 'Frames'];
 ?>
 
 <div class="<?php echo esc_attr(implode(' ', array_map('sanitize_html_class', $wrapper_classes))); ?>" data-columns="<?php echo esc_attr($columns); ?>">
-	<cloudinary-component></cloudinary-component>
+    <?php if (in_array($product->get_name(), $product_types)): ?>
+        <cloudinary-component product-type="<?php echo $product->get_name(); ?>"></cloudinary-component>
+    <?php else: ?>
+        <figure class="woocommerce-product-gallery__wrapper">
+            <?php
+            if ( $product->get_image_id() ) {
+                $html = wc_get_gallery_image_html( $post_thumbnail_id, true );
+            } else {
+                $html  = '<div class="woocommerce-product-gallery__image--placeholder">';
+                $html .= sprintf( '<img src="%s" alt="%s" class="wp-post-image" />', esc_url( wc_placeholder_img_src( 'woocommerce_single' ) ), esc_html__( 'Awaiting product image', 'woocommerce' ) );
+                $html .= '</div>';
+            }
+    
+            echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', $html, $post_thumbnail_id ); // phpcs:disable WordPress.XSS.EscapeOutput.OutputNotEscaped
+    
+            do_action( 'woocommerce_product_thumbnails' );
+            ?>
+        </figure>
+    <?php endif; ?>
 </div>
