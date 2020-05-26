ccb311335817c0bdbb2ede121509ce19ab469433
robindano <151894372+robindano@users.noreply.github.com>
2020-05-25 22:20:33 -0700

    move 'currentImage' logic to root state

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,41 +1,35 @@
-import Vue from "vue"
-import CloudinaryComponent from "./components/CloudinaryComponent.vue"
-import Cloudinary, { CldImage, CldTransformation } from "cloudinary-vue"
+import Vue from "vue";
+import CloudinaryComponent from "./components/CloudinaryComponent.vue";
+import Cloudinary, {CldImage, CldTransformation} from "cloudinary-vue";
 
 Vue.use(Cloudinary, {
     configuration: {
         cloudName: "flaunt-your-site",
     },
     components: [CldImage, CldTransformation],
-})
+});
 
-Vue.component("cloudinary-component", CloudinaryComponent)
+Vue.component("cloudinary-component", CloudinaryComponent);
 
 const app = new Vue({
     el: "#vue-app",
     data() {
         return {
+            currentImage: {},
             finalImages: [],
             cloudinaryImages: [],
-            cloudinaryTestImages: [
-                { public_id: "Utah/_DSC0873_d8bcxg.jpg" },
-                { public_id: "Utah/_DSC1086_vovwor.jpg" },
-                { public_id: "Utah/_DSC1041_jkxbgy.jpg" },
-                { public_id: "Utah/_DSC1078_ngpcke.jpg" },
-                { public_id: "Utah/_DSC0975_tyoo0t.jpg" },
-                { public_id: "Utah/_DSC1126_wkdwg2.jpg" },
-                { public_id: "Utah/_DSC0913_di0new.jpg" },
-                { public_id: "Utah/_DSC0841_w3hkst.jpg" },
-                { public_id: "Utah/_DSC1002_sijudo.jpg" },
-                { public_id: "Utah/_DSC0927_dtiu3g.jpg" },
-                { public_id: "Utah/_DSC1000_fc8j4j.jpg" },
-            ],
+            cloudinaryTestImages: require("./data.json"),
             processingImage: false,
-        }
+        };
     },
     mounted() {
         window.cloudinaryOnLoad = () => {
-            this.processingImage = false
+            this.processingImage = false;
+        };
+    },
+    updated() {
+        if (this.cloudinaryImages.length && !this.currentImage.public_id) {
+            this.currentImage = this.cloudinaryImages[0];
         }
     },
     methods: {
@@ -49,26 +43,27 @@ const app = new Vue({
                 },
                 (error, result) => {
                     if (!error && result && result.event === "success") {
-                        this.cloudinaryImages.push(result.info)
+                        this.cloudinaryImages.push(result.info);
+
                         this.finalImages.push({
                             public_id: result.info.public_id,
-                            url: result.info.secure_url
-                        })
+                            url: result.info.secure_url,
+                        });
                     }
 
                     if (error) {
-                        console.log("Cloudinary upload error")
-                        console.log(error)
+                        console.log("Cloudinary upload error");
+                        console.log(error);
                     }
                 }
-            )
+            );
 
-            myWidget.open()
+            myWidget.open();
         },
     },
     computed: {
         finalUrls() {
-            return this.finalImages.map(image => image.url)
-        }
+            return this.finalImages.map((image) => image.url);
+        },
     },
-})
+});
