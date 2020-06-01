3744863a8d546e5a610a35957fc7abc4046664eb
robindano <151894372+robindano@users.noreply.github.com>
2020-06-01 14:48:44 -0700

    php logic to handle front-end image upload

---
+++ inc/helper-functions.php
@@ -0,0 +1,47 @@
+<?php
+
+if (!function_exists('gme_image_upload')) {
+    function gme_image_upload($file, $image_size = 'thumbnail', $post_id = 0)
+    {
+        $upload = wp_upload_bits($file['name'], null, file_get_contents($file['tmp_name']));
+
+        $wp_filetype = wp_check_filetype(basename($upload['file']), null);
+
+        $attachment = [
+            'post_mime_type' => $wp_filetype['type'],
+            'post_title'     => sanitize_file_name($file['name']),
+            'post_content'   => '',
+            'post_status'    => 'inherit',
+        ];
+
+        $attach_id = wp_insert_attachment($attachment, $upload['file'], $post_id);
+
+        require_once ABSPATH . 'wp-admin/includes/image.php';
+
+        $attach_data = wp_generate_attachment_metadata($attach_id, $upload['file']);
+        wp_update_attachment_metadata($attach_id, $attach_data);
+
+        $thumbnail = wp_get_attachment_image_src($attach_id);
+        $original  = wp_get_attachment_image_src($attach_id, 'full');
+        $image     = wp_get_attachment_image_src($attach_id, $image_size);
+
+        return [
+            'id'           => $attach_id,
+            'editor_image' => [
+                'url'    => $image[0],
+                'width'  => $image[1],
+                'height' => $image[2],
+            ],
+            'thumbnail'  => [
+                'url'    => $thumbnail[0],
+                'width'  => $thumbnail[1],
+                'height' => $thumbnail[2],
+            ],
+            'original' => [
+                'url'    => $original[0],
+                'width'  => $attach_data['width'],
+                'height' => $attach_data['height'],
+            ],
+        ];
+    }
+}
--- inc/wp-enqueue.php
+++ inc/wp-enqueue.php
@@ -8,6 +8,11 @@ add_action('wp_enqueue_scripts', function () {
     if (is_product() || is_cart() && !WC()->cart->is_empty()) {
         wp_enqueue_script('cloudinary', 'https://widget.cloudinary.com/v2.0/global/all.js', ['jquery'], $version, true);
         wp_enqueue_script('woo_photo', get_stylesheet_directory_uri() . '/dist/js/app.js', ['jquery', 'cloudinary'], $version, true);
+
+        wp_localize_script('woo_photo', 'GME_DATA', [
+            'ajax_url' => admin_url('admin-ajax.php'),
+            'nonce'    => wp_create_nonce('gme_ajax_nonce'),
+        ]);
     }
 });
 
