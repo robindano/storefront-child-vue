99d2a4b9d7b22e935d52bb1ccce3c40f3691bbb0
robindano <151894372+robindano@users.noreply.github.com>
2020-06-04 08:27:02 -0700

    add gme transformations to attachment on checkout

--- inc/hooks/checkout.php
+++ inc/hooks/checkout.php
@@ -2,9 +2,18 @@
 
 // Add GME images array to order upon checkout
 add_action('woocommerce_checkout_create_order_line_item', function ($item, $cart_item_key, $values, $order) {
-    if (!isset($values['gme_image_urls'])) {
+    if (!isset($values['gme_image_data'])) {
         return;
     }
 
-    $item->add_meta_data('gme_image_urls', $values['gme_image_urls']);
+    $gme_items = $values['gme_image_data'];
+
+    // Add images transformations to attachment
+    foreach ($gme_items as $gme_item) {
+        update_post_meta(
+            $gme_item['attachment_id'], 'gme_image_transformations', $gme_item['transformations']
+        );
+    }
+
+    $item->add_meta_data('gme_image_data', $gme_items);
 }, 10, 4);
