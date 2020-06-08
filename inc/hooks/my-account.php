4e7be7a05bd1f3ea6729920cb690d04e531fe0bd
robindano <151894372+robindano@users.noreply.github.com>
2020-06-08 14:00:19 -0700

    clean up test my-account image mini grid

--- inc/hooks/my-account.php
+++ inc/hooks/my-account.php
@@ -2,14 +2,5 @@
 
 // Display GME images below each product in customer front-end 'my-account' page
 add_filter('woocommerce_display_item_meta', function ($html, $item, $args) {
-    $image_urls = $item->get_meta('gme_image_urls'); ?>
-    <div style="margin-top: 1rem;">
-        <p style="font-weight: bold; color: gray; margin-bottom: 0.1rem;">Images</p>
-        <div style="display: grid; grid-gap: 1rem; grid-template-columns: repeat(4, 1fr);">
-            <?php foreach ($image_urls as $image_url): ?>
-                <img style="width: 100%;" src="<?php echo $image_url; ?>" alt="image" />
-            <?php endforeach; ?>
-        </div>
-    </div>
-<?php
+    $gme_image_data = $item->get_meta('gme_image_data');
 }, 10, 3);
