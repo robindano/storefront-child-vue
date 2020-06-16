a303ae96d0064942380f863052f47b7a3e350b21
robindano <151894372+robindano@users.noreply.github.com>
2020-06-16 08:44:19 -0700

    remove qty validation

--- inc/hooks/cart.php
+++ inc/hooks/cart.php
@@ -1,17 +1,5 @@
 <?php
 
-// Validate item quantity being add to cart
-add_filter('woocommerce_add_to_cart_validation', function ($is_valid, $product_id, $quantity) {
-    $product_name = wc_get_product($product_id)->get_name();
-
-    // Posters quantity
-    if ($product_name === 'Posters') {
-        $is_valid = (int) $quantity % 6 === 0 ? true : false;
-    }
-
-    return $is_valid;
-}, 10, 3);
-
 // Delete GME images if cart item is removed
 add_action('woocommerce_remove_cart_item', function ($cart_item_key) {
     if (!$cart_data = WC()->cart->get_cart_item($cart_item_key)) {
