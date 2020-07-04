87d18a8c59bb28378419187c84739cf02092e199
robindano <151894372+robindano@users.noreply.github.com>
2020-07-04 16:12:37 -0700

    Poster and Vinyl templates updated.

--- inc/templates/posters-download-grid.php
+++ inc/templates/posters-download-grid.php
@@ -1,19 +1,21 @@
-<div class="poster-grid-wrapper">
+<div class="template-grid-wrapper">
 	<p>Don't have a poster design? Use one of our Photoshop templates:</p>
-	<div class="poster-grid">
+	<div class="template-grid">
 		<?php
 		if ( have_rows( 'poster_templates' ) ) :
 			while ( have_rows( 'poster_templates' ) ) :
 				the_row();
-				$thumb = get_sub_field( 'poster_thumb' );
-				$file  = get_sub_field( 'poster_file' );
+				$thumb = get_sub_field( 'template_thumb' );
+				$file  = get_sub_field( 'template_file' );
+				$name  = get_sub_field( 'template_name' )
 				?>
-				<a href="<?php echo $file['url']; ?>" download class="poster-grid_item">
+				<a href="<?php echo $file['url']; ?>" download class="template-grid_item">
 					<?php
 					if ( $thumb ) {
 						echo wp_get_attachment_image( $thumb['ID'], 'medium', false, array( 'alt' => $thumb['alt'] ) );
 					}
 					?>
+					<p><?php echo $name; ?></p>
 					<p>Download</p>
 				</a>
 			<?php endwhile; ?>
--- inc/templates/vinyl-download-grid.php
+++ inc/templates/vinyl-download-grid.php
@@ -1,5 +1,5 @@
 <div class="template-grid-wrapper">
-	<p>Don't have a poster design? Use one of our Photoshop templates:</p>
+	<p>Don't have a vinyl design? Use one of our Photoshop templates:</p>
 	<div class="template-grid">
 		<?php
 		if ( have_rows( 'vinyl_templates' ) ) :
@@ -7,6 +7,7 @@
 				the_row();
 				$thumb = get_sub_field( 'template_thumb' );
 				$file  = get_sub_field( 'template_file' );
+				$name  = get_sub_field( 'template_name' );
 				?>
 				<a href="<?php echo $file['url']; ?>" download class="template-grid_item">
 					<?php
@@ -14,6 +15,7 @@
 						echo wp_get_attachment_image( $thumb['ID'], 'medium', false, array( 'alt' => $thumb['alt'] ) );
 					}
 					?>
+										<p><?php echo $name; ?></p>
 					<p>Download</p>
 				</a>
 			<?php endwhile; ?>
