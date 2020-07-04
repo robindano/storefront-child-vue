7714881987a90ca40d39451e70f8f862992f85ba
robindano <151894372+robindano@users.noreply.github.com>
2020-07-03 17:22:26 -0700

    Added alternating blog post excerpts.

---
+++ content.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template used to display post content.
+ *
+ * @package storefront
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<hr class="wp-block-separator">
+	<div class="wp-block-columns alignwide">
+		<div class="wp-block-column">
+			<div style="display:flex;flex-direction:column;justify-content:center;height:100%;">
+				<a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
+				<?php the_excerpt(); ?>
+				<div class="wp-block-buttons aligncenter">
+					<div class="wp-block-button">
+						<a class="wp-block-button__link has-text-color has-very-dark-gray-color has-background has-cyan-bluish-gray-background-color" href="<?php the_permalink(); ?>" style="border-radius:9px">Read More</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="wp-block-column">
+			<figure class="wp-block-image size-large">
+				<?php the_post_thumbnail('large_square'); ?>
+			</figure>
+		</div>
+	</div>
+</article><!-- #post-## -->
