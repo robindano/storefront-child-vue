006edaea6a04e11b8c94613f2e698b1cb82b1f36
robindano <151894372+robindano@users.noreply.github.com>
2020-07-06 14:18:39 -0700

    Styling changes to Home and Blog.

--- content.php
+++ content.php
@@ -9,8 +9,8 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<hr class="wp-block-separator">
-	<div class="wp-block-columns alignwide">
-		<div class="wp-block-column">
+	<div class="wp-block-columns">
+		<div class="wp-block-column" style="width:66%; flex-grow:initial; flex-basis:initial;">
 			<div style="display:flex;flex-direction:column;justify-content:center;height:100%;">
 				<a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
 				<?php the_excerpt(); ?>
@@ -21,7 +21,7 @@
 				</div>
 			</div>
 		</div>
-		<div class="wp-block-column">
+		<div class="wp-block-column" style="width:33%; flex-grow:initial; flex-basis:initial;">
 			<figure class="wp-block-image size-large">
 				<?php the_post_thumbnail('large_square'); ?>
 			</figure>
