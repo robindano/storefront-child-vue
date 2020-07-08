5701d24fe87b2cfa655fe05a154130a65478e6d9
robindano <151894372+robindano@users.noreply.github.com>
2020-07-08 14:35:29 -0700

    Updating blog page layout.

--- content.php
+++ content.php
@@ -10,18 +10,18 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<hr class="wp-block-separator">
 	<div class="wp-block-columns">
-		<div class="wp-block-column" style="width:66%; flex-grow:initial; flex-basis:initial;">
+		<div class="wp-block-column" style="width:78%; flex-grow:initial; flex-basis:initial;">
 			<div style="display:flex;flex-direction:column;justify-content:center;height:100%;">
 				<a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
 				<?php the_excerpt(); ?>
 				<div class="wp-block-buttons aligncenter">
 					<div class="wp-block-button">
-						<a class="wp-block-button__link has-text-color has-very-dark-gray-color has-background has-cyan-bluish-gray-background-color" href="<?php the_permalink(); ?>" style="border-radius:9px">Read More</a>
+						<a class="wp-block-button__link has-text-color has-very-dark-gray-color has-background has-cyan-bluish-gray-background-color" href="<?php the_permalink(); ?>" style="border-radius:9px">Learn More</a>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="wp-block-column" style="width:33%; flex-grow:initial; flex-basis:initial;">
+		<div class="wp-block-column" style="width:22%; flex-grow:initial; flex-basis:initial;">
 			<figure class="wp-block-image size-large">
 				<?php the_post_thumbnail('large_square'); ?>
 			</figure>
