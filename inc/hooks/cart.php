ba25b86b9dc39a0b38cf43f40e9e46a580a6ee3e
robindano <151894372+robindano@users.noreply.github.com>
2020-07-13 21:07:07 -0700

    readd mini grid to customer cart page

--- inc/hooks/cart.php
+++ inc/hooks/cart.php
@@ -18,10 +18,21 @@ add_action('woocommerce_remove_cart_item', function ($cart_item_key) {
 // If editions prints, replace quantity input with quantity as number
 add_filter('woocommerce_cart_item_quantity', function ($product_quantity, $cart_item_key, $cart_item) {
     $print_types = ['Exhibition Prints', 'Outdoor/Vinyl Prints'];
-    
+
     if (!in_array($cart_item['data']->get_name(), $print_types)) {
         return $product_quantity;
     }
 
     return '<span>' . $cart_item['quantity'] . '</span>';
 }, 10, 3);
+
+// Display GME images below each cart item
+add_action('woocommerce_after_cart_item_name', function ($cart_item, $cart_item_key) {
+    if (!isset(['gme_image_data'][0])) {
+        return;
+    }
+
+    $gme_image_data = $cart_item['gme_image_data'];
+
+    require GME_TEMPLATES_PATH . 'gme-mini-grid.php';
+}, 10, 2);
